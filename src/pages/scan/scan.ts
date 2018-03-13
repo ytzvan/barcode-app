456b7588f33b997ec5d155709898816353c96699
Ytzvan Mastino <mastino14@gmail.com>
2018-03-13 14:14:57 -0500

    added video

--- src/pages/scan/scan.ts
+++ src/pages/scan/scan.ts
@@ -39,13 +39,7 @@ export class ScanPage {
     //Add barcode logic here
 
 
-    this._qrScanner.prepare()
-    .then((status: QRScannerStatus) => {
-       if (status.authorized) {
-         // camera permission was granted
-
-
-         // start scanning
+// start scanning
          let scanSub = this._qrScanner.scan().subscribe((text: string) => {
            console.log('Scanned something', text);
 
@@ -56,18 +50,7 @@ export class ScanPage {
          // show camera preview
          this._qrScanner.show();
 
-         // wait for user to scan something, then the observable callback will be called
-
-       } else if (status.denied) {
-         // camera permission was permanently denied
-         // you must use QRScanner.openSettings() method to guide the user to the settings page
-         // then they can grant the permission from there
-         //this._qrScanner.openSettings()
-       } else {
-         // permission was denied, but not permanently. You can ask for permission again at a later time.
-       }
-    })
-    .catch((e: any) => console.log('Error is', e));
+   
 }
   /*private goToResult(barcodeData) {
     this._nav.push(ScanResultPage, {
